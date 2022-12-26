77efc405a578d148698f40ca9b2bf4079d7d8a86
Sarp ISIK <wdsarpisik@gmail.com>
2022-12-26 16:31:15 +0100

    Edit about content

--- src/components/home-page/about.js
+++ src/components/home-page/about.js
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 
 function About() {
-  const subTitle = `text-[18px] leading-[32px] text-secondary uppercase`;
   const aboutImage = `flex justify-end relative sm:before:bg-pattern-1 sm:before:absolute sm:before:h-[336px] sm:before:w-[336px] sm:before:top-[75px] sm:before:left-[85px]`;
 
   return (
@@ -10,11 +9,25 @@ function About() {
       <div className="container max-lg:max-w-full fixed-lg:pr-0">
         <div className="lg:grid lg:grid-cols-2 max-md:flex max-md:flex-col-reverse">
           <div className="about-content max-md:pt-10">
-            <span className={subTitle}>Hakkımızda</span>
             <h3 className="text-secondary text-2xl mb-12 mt-4">
+              <b> HAKKIMIZDA:</b> <br />
               Klassmed Medikal olarak, 2011 yılında ithalat ürünleri ile
               başlayan ticari hayatımıza 2012 yılından itibaren kendi
               ürünlerimizi üreterek devam etmekteyiz.
+            </h3>
+            <h3 className="text-secondary text-2xl mb-12 mt-4">
+              <b>MİSYONUMUZ:</b> <br /> Ürün ve hizmet kalitemizi sürekli
+              geliştirerek müşteri talep ve beklentilerini en üst seviyede
+              karşılayarak, güçlü bir iletişim ile tüketicimizle aramızda
+              duygusal bir bağ oluşturmak suretiyle müşteri sadakatini sağlamak,
+              müşteri portföyümüzü büyüterek tercih edilen bir firma olmak,
+              rekabet gücümüzü ve kârlılığımızı arttırmak.
+            </h3>
+            <h3 className="text-secondary text-2xl mb-12 mt-4">
+              <b> VİZYONUMUZ:</b> <br /> Firmamızın ulusal pazarda etkinliğini
+              ve verimliliğini arttırmak amacıyla kendi uzmanlık alanımızda
+              kendi yöntemlerini sürekli geliştiren, sektörün öncülüğünü yapan,
+              Türkiye´nin güvenilir ve saygın bir kuruluşu olmak.
             </h3>
             <Link href="/about">
               <a className="boxed-btn text-[18px] leading-[30px]">Hakkımızda</a>
