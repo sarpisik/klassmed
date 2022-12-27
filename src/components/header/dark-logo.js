782904e1d766d39cfe0d5963d22b8b58285986c2
Sarp ISIK <wdsarpisik@gmail.com>
2022-12-27 10:23:19 +0100

    Edit logo size

--- src/components/header/dark-logo.js
+++ src/components/header/dark-logo.js
@@ -1,19 +1,19 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 function DarkLogo() {
-    return (
-        <Link href='/'>
-            <a className='inline-block align-middle leading-[1]'>
-                <Image
-                    src='/images/logo/logo-2.png'
-                    alt='Logo'
-                    width={195}
-                    height={70}
-                />
-            </a>
-        </Link>
-    );
+  return (
+    <Link href="/">
+      <a className="inline-block align-middle leading-[1]">
+        <Image
+          src="/images/logo/logo-2.png"
+          alt="Logo"
+          width={139}
+          height={50}
+        />
+      </a>
+    </Link>
+  );
 }
 
 export default DarkLogo;
--- src/components/header/white-logo.js
+++ src/components/header/white-logo.js
@@ -1,19 +1,14 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 function WhiteLogo() {
-    return (
-        <Link href='/'>
-            <a className='inline-block align-middle leading-[1]'>
-                <Image
-                    src='/images/logo/logo.png'
-                    alt='Logo'
-                    width={195}
-                    height={70}
-                />
-            </a>
-        </Link>
-    );
+  return (
+    <Link href="/">
+      <a className="inline-block align-middle leading-[1]">
+        <Image src="/images/logo/logo.png" alt="Logo" width={139} height={50} />
+      </a>
+    </Link>
+  );
 }
 
 export default WhiteLogo;
